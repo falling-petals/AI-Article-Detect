fe683a3f5f5ae4b9b79f915b757ff35b4b5da56e
小弟的大哥 <2627042177@qq.com>
2026-06-16 21:41:27 +0800

    init

---
+++ src/test/java/com/hzz/AiArticleDetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiArticleDetectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
